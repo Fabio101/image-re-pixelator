b63b40813365291689b35c93c3382709793cbe07
Fabio Pinto <fabio@parallel.co.za>
2016-03-24 20:20:22 +0200

    omg...

--- php/timeKeeper.php
+++ php/timeKeeper.php
@@ -2,13 +2,9 @@
 	$time = time();
 	$now = date("H:i:s", $time);
 
-	$endTime = "17:45:00";
+	$endTime = "20:25:00";
 
 	$diff = strtotime($endTime) - strtotime($now);
 	
-	if ($diff <= '0') {
-		echo '0';
-	} else {
-		echo intval($diff * 1000 / 4608, 10);
-	}
+	echo intval($diff * 1000 / 4608, 10) - 60;
 ?>
